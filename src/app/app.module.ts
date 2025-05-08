c6e59c6dee86a0c0a633d0782fc1bd2e6ca98e2b
Eduardo Adão Locks <122515706+adao-locks@users.noreply.github.com>
2025-05-08 19:07:49 -0300

    Project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { KanbanBoardComponent } from './components/kanban-board/kanban-board.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    KanbanBoardComponent,
+    TaskCardComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
